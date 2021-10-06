c8ed27a19cadb45f4dd3fcc1209a0fe4fafb7325
Gowrishankar V.V <89328469+Gowrishankarvv@users.noreply.github.com>
2021-10-06 09:16:41 +0530

    Update Even or odd number  finding c program.c

--- Even or odd number  finding c program.c
+++ Even or odd number  finding c program.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
   int num = 5;
+  printf("https://github.com/Gowrishankarvv"); 
   if(num % 2 ==0) {
     print("%d is even." , num);
   }
@@ -9,3 +10,7 @@ int main() {
   }
   return 0;
 }
+
+ /* HELLO WORLD PROGRAM */
+ /* code by gowrishankarvv */
+  
