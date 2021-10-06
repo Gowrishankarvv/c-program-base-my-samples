d3554ceb17c5047660e55827a3faeb396d398166
Gowrishankar V.V <89328469+Gowrishankarvv@users.noreply.github.com>
2021-10-06 08:59:07 +0530

    Create add 2 integers  cprogram.c

---
+++ add 2 integers  cprogram.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+         int number1, number2, sum;
+         printf(Enter 2 numbers: ");
+         scanf("%d %d", &number1 , &number2);
+         sum = number1 + number2;
+         printf("The sum is  %d" , sum);
+         return 0;
+         }
